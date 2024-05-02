9ee8cc892f98973b4619d889efd02795e4a2b825
Nanammii <kaddia@gmail.com>
2024-05-03 02:28:53 +0700

    fixed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
-import {useState, useEffect} from 'react';
-import bridge from '@vkontakte/vk-bridge';
-import {View, SplitLayout, SplitCol, ScreenSpinner} from '@vkontakte/vkui';
+
+import {View, SplitLayout, SplitCol} from '@vkontakte/vkui';
 import {useActiveVkuiLocation, usePopout} from '@vkontakte/vk-mini-apps-router';
 
-import {Persik, Home} from './panels';
+import { Home} from './panels';
 import {DEFAULT_VIEW_PANELS} from './routes';
 import {useSelector} from "react-redux";
+import {NewsDetails} from "./panels/news-details.js";
 
 export const App = () => {
   const {panel: activePanel = DEFAULT_VIEW_PANELS.HOME} = useActiveVkuiLocation();
-  // const [fetchedUser, setUser] = useState();
-  // const [popout, setPopout] = useState(<ScreenSpinner size="large"/>);
   const routerPopout = usePopout();
   const news = useSelector(state => state.news.news)
-  console.log(news)
-  //
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     // const user = await bridge.send('VKWebAppGetUserInfo');
-  //     // setUser(user);
-  //     setPopout(null);
-  //   }
-  //
-  //   fetchData();
-  // }, []);
 
   return (
     <SplitLayout popout={routerPopout}>
       <SplitCol>
         <View activePanel={activePanel}>
           <Home id="home" items={news} />
-          <Persik id="persik"/>
+          <NewsDetails />
         </View>
       </SplitCol>
     </SplitLayout>
